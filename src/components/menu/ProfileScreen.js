6cf32c0b5f2b2133dace98d6ef2ef6a649eb6fdd
oscar <oscar_sr_609@hotmail.com>
2021-04-22 01:16:22 -0500

    Datos personales y cuenta

--- src/components/menu/ProfileScreen.js
+++ src/components/menu/ProfileScreen.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
+
 import Swal from 'sweetalert2';
+
+
 import { db } from '../../firebase/firebase-config';
 import { useForm } from '../../hooks/useForm';
+import { UpdateUser } from '../user/UpdateUser';
 import { Sidebar } from './SideBar'
 
 export const ProfileScreen = () => {
@@ -11,20 +15,70 @@ export const ProfileScreen = () => {
 
     const { uid } = useSelector(state => state.auth);
 
-    const { name, fullName } = useSelector(state => state.user);
-    const [formValues, handleInputChange] = useForm({ nameU: name, fullNameU: fullName });
-    const { nameU, fullNameU } = formValues;
+
+    const {
+        name,
+        fullName,
+        birthday,
+        age,
+        sex,
+        phone1,
+        phone2,
+        rfc,
+        nationality,
+    } = useSelector(state => state.user);
+
+    const [formValues, handleInputChange] = useForm(
+        {
+            nameU: name,
+            fullNameU: fullName,
+            birthdayU: birthday,
+            ageU: age,
+            sexU: sex,
+            phone1U: phone1,
+            phone2U: phone2,
+            rfcU: rfc,
+            nationalityU: nationality,
+
+        });
+
+    const {
+        nameU,
+        fullNameU,
+        birthdayU,
+        ageU,
+        sexU,
+        phone1U,
+        phone2U,
+        rfcU,
+        nationalityU,
+    } = formValues;
+
 
     const [active, setActive] = useState(true);
     const [buttonSave, setButtonSave] = useState(false)
     const [buttonEdit, setButtonEdit] = useState(true)
 
 
+    const handleActive = () => {
+        setActive(false);
+        setButtonSave(true);
+        setButtonEdit(false);
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
         db.ref('users/' + uid).update({
             name: nameU.trim(),
             fullName: fullNameU.trim(),
+            birthday: birthdayU,
+            age: ageU,
+            sex: sexU,
+            phone1: phone1U,
+            phone2: phone2U,
+            rfc: rfcU,
+            nationality: nationalityU,
+
         }, (error) => {
             if (error) {
                 setActive(true);
@@ -40,21 +94,18 @@ export const ProfileScreen = () => {
         });
     }
 
-    const handleActive = () => {
-        setActive(false);
-        setButtonSave(true);
-        setButtonEdit(false);
-    }
 
 
 
     return (
         <>
             <Sidebar />
-
             <hr />
             <div>
-                <h1>Profile</h1>
+                <h1>Perfil</h1>
+                <UpdateUser />
+                <hr />
+                <h2>Datos personales</h2>
                 <Link
                     to="/profile"
                     className="link"
@@ -71,7 +122,7 @@ export const ProfileScreen = () => {
                         onChange={handleInputChange}
                         disabled={active}
                     />
-
+                    <br />
                     <input
                         type="text"
                         placeholder="Full Name"
@@ -82,13 +133,75 @@ export const ProfileScreen = () => {
                         disabled={active}
 
                     />
+                    <br />
+                    <input
+                        type="date"
+                        name="birthdayU"
+                        value={birthdayU}
+                        onChange={handleInputChange}
+                        disabled={active}
+
+                    />
+                    <br />
+                    <input
+                        type="text"
+                        name="ageU"
+                        value={ageU}
+                        onChange={handleInputChange}
+                        disabled={active}
+
+                    />
+                    <br />
+                    <input
+                        type="number"
+                        name="sexU"
+                        value={sexU}
+                        onChange={handleInputChange}
+                        disabled={active}
+
+                    />
+                    <br />
+                    <input
+                        type="text"
+                        name="phone1U"
+                        value={phone1U}
+                        onChange={handleInputChange}
+                        disabled={active}
+
+                    />
+                    <br />
+                    <input
+                        type="text"
+                        name="phone2U"
+                        value={phone2U}
+                        onChange={handleInputChange}
+                        disabled={active}
+                    />
+                    <br />
+                    <input
+                        type="text"
+                        name="rfcU"
+                        value={rfcU}
+                        onChange={handleInputChange}
+                        disabled={active}
+                    />
+                    <br />
+                    <input
+                        type="text"
+                        name="nationalityU"
+                        value={nationalityU}
+                        onChange={handleInputChange}
+                        disabled={active}
+                    />
+                    <br />
+
                     {
                         buttonSave &&
                         <button
                             type="submit"
                         >
                             Save
-                    </button>
+                        </button>
                     }
                 </form>
                 {
@@ -99,6 +212,8 @@ export const ProfileScreen = () => {
                         Edit
                     </button>
                 }
+
+
             </div>
         </>
     )
---
+++ src/components/user/UpdateUser.js
@@ -0,0 +1,83 @@
+import { firebase } from '../../firebase/firebase-config'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useForm } from '../../hooks/useForm'
+import { useDispatch } from 'react-redux';
+import { login } from '../../actions/auth';
+import Swal from 'sweetalert2';
+
+
+export const UpdateUser = () => {
+
+    const dispatch = useDispatch();
+
+
+    const { name, email, photoURL } = useSelector(state => state.auth)
+    const [formValues, handleInputchange] = useForm({ nameU: name });
+    const { nameU } = formValues;
+
+    const [active, setActive] = useState(true);
+    const [button, setbutton] = useState(false);
+    const [buttonU, setUbutton] = useState(true);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        firebase.auth().onAuthStateChanged((user) => {
+
+            user.updateProfile({
+                displayName: nameU
+            }).then(function () {
+                dispatch(login(user.uid, user.displayName, user.photoURL, user.email));
+                setUbutton(true);
+                setbutton(false);
+                setActive(true);
+                Swal.fire('Success', 'Data saved successfully!', 'success');
+
+            }).catch(function (error) {
+                setUbutton(true);
+                setbutton(false);
+                setActive(true);
+                Swal.fire('Error', error, 'error');
+            });
+        });
+
+    }
+
+
+    const handleActive = () => {
+        setActive(false);
+        setbutton(true);
+        setUbutton(false);
+    };
+
+    return (
+        <>
+            <h2>Mi cuenta</h2>
+            <img src={photoURL} width={100} alt={name} />
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="nameU"
+                    value={nameU}
+                    onChange={handleInputchange}
+                    disabled={active}
+                />
+                <br />
+                {
+                    button &&
+                    <button
+                        type="submit"
+                    >
+                        Save
+                </button>
+                }
+            </form>
+            {
+                buttonU &&
+                <button onClick={handleActive}>Editar</button>
+            }
+            <p>{email}</p>
+            <p>El email no es editable</p>
+        </>
+    )
+}
