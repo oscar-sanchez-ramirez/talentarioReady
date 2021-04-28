d64ed2b592b94905a2ec33275582811708fa90aa
oscar <oscar_sr_609@hotmail.com>
2021-04-28 12:05:05 -0500

    Por instalar dependecias

--- src/components/menu/HomeScreen.js
+++ src/components/menu/HomeScreen.js
@@ -1,8 +1,17 @@
 import React from 'react'
+// import { useFetch } from '../../hooks/useFetch'
 import { Sidebar } from './SideBar'
 
 export const HomeScreen = () => {
 
+    // const state = useFetch(`https://us-central1-talentario-a3d9a.cloudfunctions.net/api/user/t0mH5L55oBgsWa6raawwBNu6U3k2`,
+    //     {
+    //         mode: 'cors',
+	//         headers: {
+    //         	'Access-Control-Allow-Origin': '*',
+    //     	}
+    //     });
+    // console.log(state)
 
     return (
         <>
