8ad1203c8d95ef3d701ccbbb6e37bf38a2c48c67
oscar <oscar_sr_609@hotmail.com>
2021-05-07 14:02:34 -0500

    Upload master

---
+++ src/components/test/SelfRecognition.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const SelfRecognition = () => {
+    return (
+        <div>
+           <h5>Auto Reconocimiento</h5>
+           <form>
+               <input type="number" />
+           </form>
+        </div>
+    )
+}
--- src/components/user/CalculateTest.js
+++ src/components/user/CalculateTest.js
@@ -1,60 +1,54 @@
 import React from 'react'
 import { Sidebar } from '../menu/SideBar'
+import { SelfRecognition } from '../test/SelfRecognition';
 
 export const CalculateTest = () => {
 
-    var url = 'https://us-central1-talentario-a3d9a.cloudfunctions.net/api/calculateTestResults';
-    var data = {
+    // var data = {
 
-        selfRecognition: 6,
-        selfEsteem: 6,
-        procrastinationControl: 6,
-        victimizationControl: 6,
-        adaptability: 5,
+    //     selfRecognition: 6,
+    //     selfEsteem: 6,
+    //     procrastinationControl: 6,
+    //     victimizationControl: 6,
+    //     adaptability: 5,
 
-        observation: 5,
-        creativityInnovation: 5,
-        entrepreneurshipInitiative: 4,
-        improvisation: 4,
-        problemSolving: 4,
+    //     observation: 5,
+    //     creativityInnovation: 5,
+    //     entrepreneurshipInitiative: 4,
+    //     improvisation: 4,
+    //     problemSolving: 4,
 
-        emotionalIntelligence: 3,
-        rationalIntelligence: 3,
-        focus: 3,
-        collaboration: 3,
-        negotiation: 2,
+    //     emotionalIntelligence: 3,
+    //     rationalIntelligence: 3,
+    //     focus: 3,
+    //     collaboration: 3,
+    //     negotiation: 2,
 
-        selfConfidence: 2,
-        frustrationTolerance: 2,
-        assertiveCommunication: 1,
-        conflictManagement: 1,
-        resilience: 1,
+    //     selfConfidence: 2,
+    //     frustrationTolerance: 2,
+    //     assertiveCommunication: 1,
+    //     conflictManagement: 1,
+    //     resilience: 1,
 
-        leadership: 7,
-        reliability: 7,
-        teamWork: 7,
-        belongingSense: 7,
-        empathy: 7
-    };
-
+    //     leadership: 7,
+    //     reliability: 7,
+    //     teamWork: 7,
+    //     belongingSense: 7,
+    //     empathy: 7
+    // };
 
 
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data), // JSON.stringify(data)
-        headers: {
-            'Accept': 'application/html,application/text,application/json',
-            'Content-Type': 'application/json,application/html,application/text'
-        },
-    }).then(response => console.log(response))
-        .catch(error => console.log(error))
 
 
     return (
         <div>
             <Sidebar />
             <hr />
-            <h5>Test</h5>
+            <h4>Evaluación de Talentos</h4>
+            <br />
+            <h5>Asumir la responsabilidad propia (PA)</h5>
+            <br />
+            <SelfRecognition />
         </div>
     )
 }
