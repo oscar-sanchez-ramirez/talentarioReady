3838f7c33da48d479d7239212b9db59fb9501215
oscar <oscar_sr_609@hotmail.com>
2021-05-10 13:34:50 -0500

    Import bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom';
 import './styles/styles.scss'
 import { TalentarioApp } from './TalentarioApp';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
 
 ReactDOM.render(
   <TalentarioApp />,
