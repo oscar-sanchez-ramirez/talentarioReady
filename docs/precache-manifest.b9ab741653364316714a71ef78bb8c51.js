4c2eb815d264b027c6c91eab39551c58b624f937
oscar <oscar_sr_609@hotmail.com>
2021-05-11 23:30:49 -0500

    Add docs

---
+++ docs/precache-manifest.b9ab741653364316714a71ef78bb8c51.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcda2b627a3593869fbfc76318cff9dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1a0cf7fd4cf8354efb5",
+    "url": "/static/css/2.463bc4b4.chunk.css"
+  },
+  {
+    "revision": "5c1857b7c7e05a623c69",
+    "url": "/static/css/main.b3d4f0f7.chunk.css"
+  },
+  {
+    "revision": "d1a0cf7fd4cf8354efb5",
+    "url": "/static/js/2.5a108b71.chunk.js"
+  },
+  {
+    "revision": "43201e6b026fd73d0feb05180bb56588",
+    "url": "/static/js/2.5a108b71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c1857b7c7e05a623c69",
+    "url": "/static/js/main.b06915b5.chunk.js"
+  },
+  {
+    "revision": "ef59e5ac120078605b5b",
+    "url": "/static/js/runtime-main.b0c11435.js"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "73235c25bb069415797d7c862d2f97cd",
+    "url": "/static/media/bg-form_body.73235c25.png"
+  },
+  {
+    "revision": "a8c7c5e2ec73bc30c324035e9c8a264e",
+    "url": "/static/media/bg-form_login.a8c7c5e2.jpg"
+  }
+]);
