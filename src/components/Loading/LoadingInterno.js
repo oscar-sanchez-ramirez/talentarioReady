f5d76288fe6d3f649343485ee0ffb3c5189b49e7
Marcos <desarrollo1@solucionesim.net>
2021-07-08 10:55:18 -0500

    Cambio de Loading

--- src/components/Loading/LoadingInterno.js
+++ src/components/Loading/LoadingInterno.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 export const LoadingInterno = () => {
     return (
-        <div>
-            
+        <div className="body-load-int">
+            <div className="wrapper">
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="shadow-circle"></div>
+                <div className="shadow-circle"></div>
+                <div className="shadow-circle"></div>
+                <span>Cargando</span>
+            </div>
         </div>
     )
 }
